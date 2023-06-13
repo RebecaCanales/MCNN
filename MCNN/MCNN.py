e046cbbd7783a091f05919b3c09b59573169069c
Martha Rebeca Canales Fiscal <marthar.canales@gmail.com>
2023-06-12 19:17:31 -0600

    Update MCNN.py

--- MCNN/MCNN.py
+++ MCNN/MCNN.py
@@ -1,132 +1,68 @@
-import torch
-import torch.utils.data.dataloader
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from morph2d import Morph2d
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from MoorePenrose import MoorePenrose
-from Dilation2d import Dilation2d
 
 class MCNN(nn.Module):
+    """Morphological-convolutiona neural network (MCNN) class"""
     def __init__(self):
         super(MCNN, self).__init__()
-        # Layer operations
-        self.conv1 = nn.Conv2d(1, 15, kernel_size=5, padding=1)
-        self.conv2 = nn.Conv2d(15, 210, kernel_size=5, padding=1)
-        self.dil1 = Dilation2d(1, 15, kernel_size=5, padding=1)
-        self.fc2 = nn.Linear(36*210, 10, bias=False)
         
-        self.op_weights = []
-        self.optimizer = MoorePenrose(params=self.parameters(),C=1e-3)
-        
-    def arrange_input_target(self, weight, stride, inputs):
-        num_w, depth_w, height_w, width_w = weight.shape
-        xx = []
-        
-        for img_index in range(0,inputs.shape[0]):
-            for row_index in range(0, inputs.shape[2]-height_w, stride):
-                for col_index in range(0, inputs.shape[3]-width_w, stride):
-                    local_win = torch.flatten(inputs[img_index,:,row_index:height_w + row_index,col_index:width_w + col_index])
-                    xx.append(local_win)
-    
-        self.X = torch.stack(xx)
-        Xn = (self.X-torch.mean(self.X))/torch.std(self.X, unbiased=False)
-        intercept = torch.ones(Xn.shape[0],1)
-        self.T = torch.cat((Xn, intercept), 1)
-        #return X, T #global
-    
-    def arrange_weights(self, weight):
-        row_x, col_x = self.X.shape
-        num_w, depth_w, height_w, width_w = weight.shape
-        self.W = torch.zeros([height_w*width_w*depth_w, num_w])
-    
-        for i in range(num_w):
-            self.W[:,i] = torch.flatten(weight[i, :, :, :])
-        
-        self.b = torch.rand([row_x,1])
-        #return W, b #global
-    
-    def arreange_optimal_weights(self, optimal_weights,shape):
-        Fmat = optimal_weights[:-1,:]
-        B = optimal_weights[-1:,:]
-        Fmat = torch.transpose(Fmat, 0, 1)
-        B = torch.transpose(B, 0, 1)
-        self.w = torch.reshape(Fmat, shape)
-        nn.Conv2d.weight = nn.Parameter(self.w)
-        self.op_weights.append(nn.Conv2d.weight)
-        #return w #global
-    
-    def TrainConvLayer(self, inputs, weight, stride):
-        #X, T = arrange_input(weight, stride=1, inputs=x_ini) #global
-        self.arrange_input_target(weight,1,inputs)
-        #W, b = arrange_weights(X, weight) #global
-        self.arrange_weights(weight)
-        H_int = torch.mm(self.X, self.W) + self.b
-        self.H = F.relu(H_int)
-        #return H, T #global
-    
-    def forwardConvTraining(self, x, weight, stride):
-        
-        finalLayer = False
-        # Training convolution
-        #H,T = TrainConvLayer(x, weight, stride) #global
-        shape = weight[0].shape
-        self.TrainConvLayer(x, weight[0], stride)
-        optimal_weights = self.optimizer.train(self.H,self.T,finalLayer)
-        #w = arreange_optimal_weights(optimal_weghts) #global
-        self.arreange_optimal_weights(optimal_weights,shape)
-        
-        # First trained layer
-        x = self.dil1(x) # temporal change self.conv1(x) --> self.dil1
-        x = F.relu(x)
+        self.morph = Morph2d(1, 4, kernel_size=5,
+                               dilation=True,
+                               erosion=False,
+                               convolution=False,
+                               sequence= False, # ("dilation", "erosion")
+                               subtraction= ("original", "erosion")) # ("original", "erosion")
+        self.conv1 = nn.Conv2d(4, 20, kernel_size=5, padding="same")
+        self.conv2 = nn.Conv2d(20, 5, kernel_size=5, padding="same")
+        self.fc = nn.Linear(1024*5, 1, bias=False)
+        self.act = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        """Complete process"""
+        x = self.morph(x).float()
         x = F.max_pool2d(x,kernel_size=2)
-        
-        shape = weight[2].shape
-        # Training convolution2
-        #H,T = TrainConvLayer(x, weight, stride) #global
-        self.TrainConvLayer(x, weight[2], stride)
-        optimal_weights = self.optimizer.train(self.H,self.T, finalLayer)
-        #w = arreange_optimal_weights(optimal_weghts) #global
-        self.arreange_optimal_weights(optimal_weights,shape)
-        
-        # Second trained layer
-        par = list(self.parameters())
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = F.max_pool2d(x,kernel_size=2)
+        x = F.relu(x)
         x = self.conv2(x)
-        x = F.relu(x)
         x = F.max_pool2d(x,kernel_size=2)
-        
-        return x
-    
-    def forwardLastLayer(self, x, target):
-        
-        # Fully connected
-        finalLayer = True
+        x = F.relu(x)
+
+
         x = x.view(-1, self.num_flat_features(x))
-        optimal_weights = self.optimizer.train(x,target, finalLayer)
-        nn.Linear.weight = optimal_weights
-        self.op_weights.append(nn.Linear.weight)
-        x = self.fc2(x)
+
+        x = self.fc(x)
+        x = self.act(x)
 
         return x
-    
+
+    def forwardToHidden(self, x):
+        """Process stopped at hidden layer - before fully connected -.
+        This process calculate the hidden output used to estimate the weights"""
+        x = self.morph(x).float()
+        x = F.max_pool2d(x,kernel_size=2)
+        x = self.conv1(x)
+        x = F.max_pool2d(x,kernel_size=2)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x,kernel_size=2)
+        x = F.relu(x)
+        x = x.view(-1, self.num_flat_features(x))
+        
+        return x
+
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
         return num_features
-    
-    def forward(self, x):
-        nn.Conv2d.weight = self.op_weights[0]
-        x = self.dil1(x) # temporal change self.conv1(x) --> self.dil1(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x,kernel_size=2)
-        
-        nn.Conv2d.weight = self.op_weights[1]
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x,kernel_size=2)
-        
-        x = x.view(-1, self.num_flat_features(x))
-        x = self.fc2(x)
-        
-        return x
+
